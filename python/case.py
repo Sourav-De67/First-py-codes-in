c21205a8908625f74afa185598b95622872657c1
Sourav De <souravdey721650@gmail.com>
2026-07-24 20:38:19 +0530

    leap year

--- python/case.py
+++ python/case.py
@@ -1,5 +1,7 @@
 ch=input('Enter a single charecter: ')
-if ch.isupper():
+if len(ch)!=1:
+    print("Enter a single charecter")
+elif ch.isupper():
     print(f"{ch} is a upper case charecter")
 elif ch.islower():
     print(f"{ch} is a lower case charecter")
