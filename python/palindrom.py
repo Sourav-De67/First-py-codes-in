f19a348655b61816c6f9d2caab75b1523603f773
Sourav De <souravdey721650@gmail.com>
2026-07-31 20:25:34 +0530

    prime number

--- python/palindrom.py
+++ python/palindrom.py
@@ -1,4 +1,4 @@
-a=int(input('Enter a number :'))
+'''a=int(input('Enter a number :'))
 c=a
 rev=0
 while a>0:
@@ -8,4 +8,18 @@ while a>0:
 if c==rev:
     print("Palindrom")
 else:
-    print("Not palindrom")
+    print("Not palindrom")'''
+def pall(a):
+    rev=0
+    c=a
+    while a>0:
+        digit=a%10
+        rev=rev*10+digit
+        a=a//10
+    if c==rev:
+        print("Palindrom")
+    else:
+        print("Not palindrom")
+        
+pall(121)
+pall(142)
