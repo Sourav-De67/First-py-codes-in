ac65a25c84f6c3efe79c60e3978248c0ba52ac77
Sourav De <souravdey721650@gmail.com>
2026-08-03 19:43:28 +0530

    sorting check

---
+++ python/sorting.py
@@ -0,0 +1,9 @@
+a=[1,2,3,23,4]
+for i in range (len(a)-1):
+    if a[i]<a[i+1]:
+        continue
+    else:
+        print("Your list is not sorted")
+        break
+else:
+    print("Your list is sorted")
