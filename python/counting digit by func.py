fca3c3e1fcb83ba6c771871400b167e3cec3763b
Sourav De <souravdey721650@gmail.com>
2026-08-07 20:48:04 +0530

    counting digti by function

---
+++ python/counting digit by func.py
@@ -0,0 +1,10 @@
+def count(n):
+    count=0
+    if n==0:
+        print('number of digit is 1')
+    else:
+        while n>0:
+            n=n//10
+            count=count+1
+    print('Number of digit is',count)
+count(1234)
